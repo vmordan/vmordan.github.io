d06a7a7dd2cdcd0092c01d327f0f0d731c60aa3d
Mordan Vitalii <vitm.base@gmail.com>
2026-02-17 20:43:37 -0300

    move control text strings to a dedicated file
    Centralize all control text strings in a single file to improve
    reusability and simplify translations.

---
+++ src/config/control.ts
@@ -0,0 +1,40 @@
+// This file contains name of the control elements, which can be changed for different meaning or for translations.
+
+// Main lang
+export const LANG = "en"
+
+// Main panel
+export const NAME_HOME = "Home"
+export const NAME_PUBLICATIONS = "Publications"
+export const NAME_PROJECTS = "Projects"
+export const NAME_TALKS = "Talks"
+export const NAME_ABOUT = "About"
+
+// Links
+export const NAME_EMAIL = "Email"
+export const NAME_GITHUB = "GitHub"
+export const NAME_SCHOLAR = "Google Scholar"
+export const NAME_LINKEDIN = "LinkedIn"
+export const NAME_ORCID = "ORCID"
+export const NAME_CONTACT = "Contact"
+
+// Page text
+export const NAME_LINKS = "Useful links"
+export const NAME_SELECTED_PUBLICATIONS = "Selected publications"
+export const NAME_SELECTED_PROJECTS = "Selected projects"
+export const NAME_OUTSIDE_WORK = "Outside work"
+export const NAME_VIEW_ALL = "View all"
+
+export const NAME_PHOTO_GALLERY = "Photo gallery"
+export const NAME_PHOTO_GALLERY_DESC = "A small selection"
+
+export const NAME_ITEMS = "items"
+export const NAME_PDF = "PDF"
+export const NAME_DETAILS = "Details"
+export const NAME_DOI = "DOI"
+export const NAME_LINK = "Link"
+export const NAME_VIDEO = "Video"
+export const NAME_ARTIFACT = "Artifact"
+export const NAME_AUTHORS = "Authors"
+export const NAME_CODE = "Code"
+
--- src/config/site.ts
+++ src/config/site.ts
@@ -15,3 +15,9 @@ export const HOME_PAGE_OUTSIDE_WORK_CHIPS = [
 export const ABOUT_BIO = [
   "Short bio"
 ]
+
+export const DESC_ABOUT = "Short bio, and a small photo gallery"
+export const DESC_HOME = "Personal web site"
+export const DESC_PUBLICATIONS = "Selected publications and related materials (PDFs, links, videos)"
+export const DESC_PROJECTS= "Selected projects and contributions with links to repositories and materials"
+export const DESC_TALKS = "Selected talks and related materials (PDFs, links, videos)"
